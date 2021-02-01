0a0fcc0481a03743f8d101b50ab8d4a03dea6e6e
Olga-Kulagina <olga.kulagina17@yandex.ru>
2021-02-01 15:11:58 +0300

    refactor AddItemForm

--- src/common/AddItemForm/AddItemForm.tsx
+++ src/common/AddItemForm/AddItemForm.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, MouseEvent, useState} from 'react';
+import React, {ChangeEvent, MouseEvent, useState, KeyboardEvent} from 'react';
 import {Button, Input} from 'antd';
 import s from './AddItemForm.module.css'
 import {PlusOutlined} from '@ant-design/icons';
@@ -17,11 +17,19 @@ export const AddItemForm = (props: AddItemFormPropsType) => {
 
     const onClickHandler = (e: MouseEvent<HTMLButtonElement>) => {
         props.callback(title)
+        setTitle('')
+    }
+
+    const onEnterHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if(e.charCode === 13){
+            props.callback(title)
+            setTitle('')
+        }
     }
 
     return (
         <div className={s.addItemForm}>
-            <Input type='text' onChange={onChangeHandler} style={{width: 200}}/>
+            <Input type='text' value={title} onChange={onChangeHandler} onKeyPress={onEnterHandler} style={{width: 200}}/>
             <Button onClick={onClickHandler} type='primary' size='small' shape="circle"
                     style={{margin: '0 5px'}} icon={<PlusOutlined />} />
         </div>
