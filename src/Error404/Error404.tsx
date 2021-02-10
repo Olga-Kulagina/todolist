2f21679b2694f11af10baff5d8bcf5b1755d435f
Olga-Kulagina <olga.kulagina17@yandex.ru>
2021-02-10 10:16:27 +0300

    add error404 page

--- src/Error404/Error404.tsx
+++ src/Error404/Error404.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import {Button, Result} from 'antd';
+import {NavLink} from 'react-router-dom';
+import {PATH} from '../App';
 
 export const Error404 = () => {
     return (
         <div>
-            404
+            <Result
+                status="404"
+                title="404"
+                subTitle="Sorry, the page you visited does not exist."
+                extra={<Button type="primary"><NavLink to={PATH.TODOS}>Back Home</NavLink></Button>}
+            />
         </div>
     )
 }
